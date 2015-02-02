e8a05311793be3eeca4c9bf1a193249ae69f6274
suicidepills <atom.mac@gmail.com>
2015-02-01 23:38:18 -0500

    Add multiple death screen messages.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/death-state.js
+++ js/death-state.js
@@ -1,9 +1,12 @@
-define(['lib/jaws', 'score-keeper'], function (jaws, ScoreKeeper) {
+define(['lib/jaws', 'score-keeper', 'utils'], function (jaws, ScoreKeeper, utils) {
 
 return {
 	alpha: 0,
+	shame: ['Are you kidding me?!', 'Well, at least you tried.  I guess.', 'Well, that was shameful.'],
+	message: null,
 	setup: function () {
 		this.alpha = 0;
+		this.message = this.shame[utils.randomInt(0, this.shame.length-1)];
 	},
 	update: function () {},
 	draw: function () {
@@ -15,16 +18,19 @@ return {
 			jaws.previous_game_state.draw();
 			
 			var context = jaws.context;
+
+			// context.globalCompositeOperation = 'lighter';
 			context.fillStyle = '#FF6C3A';
 			context.rect(0, 0, jaws.width, jaws.height);
-			context.globalAlpha = this.alpha * 0.85;
+			context.globalAlpha = this.alpha * 0.7;
 			context.fill();
 			context.globalAlpha = 1;
+			// context.globalCompositeOperation = 'source-over';
 
-			context.fillStyle = '#FFBE3A';
+			context.fillStyle = '#000';
 			context.font = '25px Arial';
 			context.textAlign = 'center';
-			context.fillText('YOU FUCKIN\' DIED!', jaws.canvas.width / 2, jaws.canvas.height / 2);
+			context.fillText(this.message, jaws.canvas.width / 2, jaws.canvas.height / 2);
 			
 			context.font = '20px Arial';
 			if(ScoreKeeper.isHighScore()) {
